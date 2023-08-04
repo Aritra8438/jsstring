dd341e3c713cf43ee5f3a2fc617415f5a133490e
Aritra Majumder <aritramajumder8438@gmail.com>
2023-08-05 00:30:02 +0530

    Revert back changes to pass linting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 String.prototype.capitalize = function () {
   return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
 };
-ab = "Hi";
+
 String.prototype.casefold = function () {
   return this.toLowerCase();
 };
