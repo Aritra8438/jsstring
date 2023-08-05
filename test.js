74e50a9ed04fb9698240c3ea3e3b346ee28b997e
Aritra Majumder <aritramajumder8438@gmail.com>
2023-08-05 22:03:27 +0530

    Add python `index` method and doc-strings

--- test.js
+++ test.js
@@ -6,16 +6,82 @@ test("capitalize method capitalizes the first letter of the string and convert o
   var inputString = "hello world";
   var expectedOutput = "Hello world";
   expect(inputString.capitalize()).toBe(expectedOutput);
+
   inputString = "hello World";
   expectedOutput = "Hello world";
   expect(inputString.capitalize()).toBe(expectedOutput);
 });
 
-test("casefold method convert every letter to lowercase", () => {
+test("casefold method converts every letter to lowercase", () => {
   var inputString = "Hello World";
   var expectedOutput = "hello world";
   expect(inputString.casefold()).toBe(expectedOutput);
+
   inputString = "HELLO WORLD";
   expectedOutput = "hello world";
   expect(inputString.casefold()).toBe(expectedOutput);
+});
+
+test.only("index method returns the first occurrence of the substring in the given range", () => {
+  var inputString = "Hello world";
+  var expectedValueError = "substring not found";
+
+  var stringToSearch = "";
+  var expectedOutput = 0;
+  expect(inputString.index(stringToSearch)).toBe(expectedOutput);
+
+  stringToSearch = "H";
+  expectedOutput = 0;
+  expect(inputString.index(stringToSearch)).toBe(expectedOutput);
+
+  stringToSearch = "h";
+  expect(() => inputString.index(stringToSearch)).toThrow(expectedValueError);
+
+  stringToSearch = "H";
+  expect(() => inputString.index(stringToSearch, 1)).toThrow(
+    expectedValueError
+  );
+
+  stringToSearch = "e";
+  expectedOutput = 1;
+  expect(inputString.index(stringToSearch, 1, 2)).toBe(expectedOutput);
+
+  stringToSearch = "e";
+  expect(() => inputString.index(stringToSearch, 0, 1)).toThrow(
+    expectedValueError
+  );
+
+  stringToSearch = "o";
+  expectedOutput = 7;
+  expect(inputString.index(stringToSearch, 5)).toBe(expectedOutput);
+
+  stringToSearch = "o";
+  expectedOutput = 7;
+  expect(() => inputString.index(stringToSearch, 5, 7)).toThrow(
+    expectedValueError
+  );
+
+  stringToSearch = "Hello";
+  expectedOutput = 0;
+  expect(inputString.index(stringToSearch)).toBe(expectedOutput);
+
+  stringToSearch = "ello";
+  expectedOutput = 1;
+  expect(inputString.index(stringToSearch)).toBe(expectedOutput);
+
+  stringToSearch = "world";
+  expectedOutput = 6;
+  expect(inputString.index(stringToSearch)).toBe(expectedOutput);
+
+  stringToSearch = " w";
+  expectedOutput = 5;
+  expect(inputString.index(stringToSearch)).toBe(expectedOutput);
+
+  stringToSearch = "H";
+  expect(() => inputString.index(stringToSearch, 0, 0)).toThrow(
+    expectedValueError
+  );
+
+  stringToSearch = "word";
+  expect(() => inputString.index(stringToSearch)).toThrow(expectedValueError);
 });
